fb57a35448abd561bb214e032d004feae83812b0
Maarten Bobbeldijk <maarten@bobbeldijk.nl>
2022-12-09 19:33:25 +0100

    Added solution for day 5

---
+++ src/main/java/nl/bobbeldijk/day5/part2/Application.kt
@@ -0,0 +1,39 @@
+package nl.bobbeldijk.day5.part2
+
+import nl.bobbeldijk.util.Answerable
+import nl.bobbeldijk.util.InputFile
+import nl.bobbeldijk.util.InputReader
+
+fun main() {
+    Application().calculateAnswer(InputReader.readListFromInputFile(InputFile.DAY5))
+}
+
+data class Command(val from: Int, val to: Int, val amount: Int)
+
+class Application : Answerable<String> {
+    override fun calculateAnswer(input: MutableList<String>): String {
+        val initState = input.takeWhile(String::isNotEmpty).toMutableList()
+        val commands = input.subList(initState.size + 1, input.size)
+
+        val stacks = (0 until initState.maxOf { it.length }).map { ArrayDeque<Char>(0) }
+        initState.forEach {
+            it.mapIndexed { index, char -> Pair(index, char) }
+                .filter { pair -> pair.second.isLetter() }
+                .forEach { pair -> stacks[pair.first].addFirst(pair.second) }
+        }
+
+        commands.map { Regex("\\d+").findAll(it).map { match -> match.value.toInt() }.toList() }
+            .map { Command(it[1] - 1, it[2] - 1, it[0]) }
+            .forEach { command ->
+                (0 until command.amount)
+                    .map { stacks[command.from].removeLast() }
+                    .reversed()
+                    .forEach { stacks[command.to].addLast(it) }
+            }
+
+        val answer = stacks.joinToString("", transform = { it.last().toString() })
+        println("Answer: $answer")
+
+        return answer
+    }
+}
---
+++ src/test/java/nl/bobbeldijk/day5/part2/ApplicationTest.java
@@ -0,0 +1,30 @@
+package nl.bobbeldijk.day5.part2;
+
+import nl.bobbeldijk.util.AnswerNotFoundException;
+import nl.bobbeldijk.util.InputFile;
+import nl.bobbeldijk.util.InputReader;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+class ApplicationTest {
+
+    private Application application;
+
+    @BeforeEach
+    void setUp() {
+        application = new Application();
+    }
+
+    @Test
+    void calculateAnswerTest() throws AnswerNotFoundException {
+        var testInput = InputReader.readStreamFromInputFile(InputFile.DAY5, true, "test")
+                .collect(Collectors.toList());
+
+        assertEquals("MCD", application.calculateAnswer(testInput));
+    }
+}
